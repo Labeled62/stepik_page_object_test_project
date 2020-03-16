df271e829abb68ddc9adc262d97c23a04695e856
Labeled62 <dusya.69@mail.ru>
2020-03-16 09:58:34 +0300

    add base page and create main page
    base page:
    1.create .py
    2. add open browser constructor
    3. add get page method
    main page:
    1. create .py

---
+++ base_page.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium import webdriver
+import time
+import math
+
+class BasePage():
+    def __init__(self, browser, url):
+        self.browser=browser
+        self.url=url
+    def open(self):
+        self.browser.get(self.url)
