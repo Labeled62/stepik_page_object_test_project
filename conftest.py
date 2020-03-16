7dbedf124ec43347991086ec33c02feaa82afb2a
Labeled62 <dusya.69@mail.ru>
2020-03-16 09:21:23 +0300

    create files and "start" commit
    create empty project files, add configure-files and block 4 started!

---
+++ conftest.py
@@ -0,0 +1,33 @@
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                 help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="en",
+                 help="Choose language: ar, ca, cs, da, de, en-gb, el, es, fi, fr, it, ko, nl, pl, pt, pt-br, ro, ru, sk, uk, zh-hans")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language  =str(request.config.getoption("language")  )
+    browser_name = request.config.getoption("browser_name")
+    if browser_name == "chrome":
+        print("\nstart chrome browser with for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+        print("\nstart firefox browser for test..")
+    else:
+        print("Browser {} is not corrected".format(browser_name))
+        print \
+            ("Language must be one of: ar, ca, cs, da, de, en-gb, el, es, fi, fr, it, ko, nl, pl, pt, pt-br, ro, ru, sk, uk, zh-hans")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
