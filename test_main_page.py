6b94a02319ee246cd674c3816dfe254c7c8e1243
Labeled62 <dusya.69@mail.ru>
2020-03-16 10:11:10 +0300

    add login test on page_object model
    main page:
    1. add method find and click login page
    test_main_page:
    1. refactor code on main page and page object model

--- test_main_page.py
+++ test_main_page.py
@@ -2,10 +2,15 @@ import pytest
 from selenium import webdriver
 import time
 import math
+from .main_page import MainPage
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+
+def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
